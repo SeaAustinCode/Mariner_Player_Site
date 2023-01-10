9ed173cf4d9c88e3fa1beadd1e655d9e46f6b26e
SeaAustinCode <seaaustincode@gmail.com>
2023-01-10 11:55:50 -0800

    change to model --- added legend status

--- server/model/player.js
+++ server/model/player.js
@@ -48,34 +48,38 @@ const PlayerSchema = new mongoose.Schema({
         required: [true, "{PATH} must be linked to a player"],
     },
     awards: {
-            type: String,
-            default: "Player has not earned any awards yet",
+        type: String,
+        default: "Player has not earned any awards yet",
     },
-        infield: {
-            type: Boolean,
-            required: [true, "{PATH} must be linked to a player"],
-        },
+    legend: {
+        type: String,
+        required: [true, "{PATH} status must be designated for player"],
+    },
+    infield: {
+        type: Boolean,
+        required: [true, "{PATH} must be linked to a player"],
+    },
 
-        outfield: {
-            type: Boolean,
-            required: [true, "{PATH} must be linked to a player"],
-        },
+    outfield: {
+        type: Boolean,
+        required: [true, "{PATH} must be linked to a player"],
+    },
 
-        startingPitcher: {
-            type: Boolean,
-            required: [true, "{PATH} must be linked to a player"],
-        },
+    startingPitcher: {
+        type: Boolean,
+        required: [true, "{PATH} must be linked to a player"],
+    },
 
-        reliefPitcher: {
-            type: Boolean,
-            required: [true, "{PATH} must be linked to a player"],
-        },
+    reliefPitcher: {
+        type: Boolean,
+        required: [true, "{PATH} must be linked to a player"],
+    },
 
-        playerOfficialSite: {
-            type: String,
-            default: "Player does not have an official website",
-        },
-    }, { timestamps: true })
+    playerOfficialSite: {
+        type: String,
+        default: "Player does not have an official website",
+    },
+}, { timestamps: true })
 
 //create the schema and export it 
 const Player = mongoose.model('Player', PlayerSchema);
